ebfb59c99e2d813ebd3c86dc944d2c704a926414
Sean Spradlin <sts@smartvue.com>
2016-07-23 13:15:46 -0500

    Message when service launched

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.static(`${__dirname}/../public`));
-app.listen(8080);
+app.listen(8080, () => {
+  console.log(`Server running at 8080`);
+});
+
