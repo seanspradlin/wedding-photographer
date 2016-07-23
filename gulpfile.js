a0ce2ab074cf35425ca67f0888b9c45c6df1e79d
Sean Spradlin <sts@smartvue.com>
2016-07-23 13:15:26 -0500

    Added jquery and bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,27 @@ gulp.task('js-vendor', () => {
   gulp
     .src([
       'node_modules/vue/dist/vue.*',
+      'node_modules/bootstrap/dist/js/*.*',
+      'node_modules/jquery/dist/jquery.*',
     ])
-    .pipe(gulp.dest('public/js/vendor'));
+    .pipe(gulp.dest('public/vendor/js'));
 });
 
-gulp.task('default', ['js-vendor']);
+gulp.task('css-vendor', () => {
+  gulp
+    .src([
+      'node_modules/bootstrap/dist/css/*.*',
+    ])
+    .pipe(gulp.dest('public/vendor/css'));
+});
+
+gulp.task('fonts-vendor', () => {
+  gulp
+    .src([
+      'node_modules/bootstrap/dist/fonts/*.*',
+    ])
+    .pipe(gulp.dest('public/vendor/fonts'));
+});
+
+gulp.task('default', ['js-vendor', 'css-vendor', 'fonts-vendor']);
 
