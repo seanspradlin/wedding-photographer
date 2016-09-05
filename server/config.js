5ad132892afb8eb3c7d1f79927ef666c9b9b3e31
Sean Spradlin <seanspradlin@gmail.com>
2016-09-05 13:51:10 -0500

    added log level and facebook to config

--- server/config.js
+++ server/config.js
@@ -3,6 +3,12 @@ module.exports = {
   db: {
     url: process.env.MONGO_URL || 'mongodb://localhost/wedding-photographer',
   },
+  facebook: {
+    clientId: process.env.FACEBOOK_CLIENT_ID || '',
+    clientSecret: process.env.FACEBOOK_CLIENT_SECRET || '',
+    callbackUrl: process.env.FACEBOOK_CALLBACK_URL || '',
+  },
+  logLevel: process.env.LOG_LEVEL || 'debug',
   session: {
     secret: process.env.SESSION_SECRET || 'hunter2',
     resave: false,
