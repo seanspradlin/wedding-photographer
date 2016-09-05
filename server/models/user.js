d55d8f16170012c553a910f6685e7dfd1d2cfb5b
Sean Spradlin <seanspradlin@gmail.com>
2016-09-04 19:54:30 -0500

    give users local facebook and google extensions

--- server/models/user.js
+++ server/models/user.js
@@ -1,8 +1,32 @@
 'use strict';
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+
 const userSchema = mongoose.Schema({
-  email: String,
+  local: {
+    name: String,
+    email: String,
+    password: String,
+  },
+  facebook: {
+    id: String,
+    token: String,
+    email: String,
+    name: String,
+  },
+  google: {
+    id: String,
+    token: String,
+    email: String,
+    name: String,
+  },
 });
+
+userSchema.methodsd.generateHash =
+  password => bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+
+userSchema.methods.validPassword =
+  password => bcrypt.compareSync(password, this.local.password);
 
 module.exports = mongoose.model('User', userSchema);
 
