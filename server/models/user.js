1b8ee1fe09c219486032805a43f501da4afb76e3
Sean Spradlin <seanspradlin@gmail.com>
2016-09-05 12:27:18 -0500

    Functions not attributing because of lexical this

--- server/models/user.js
+++ server/models/user.js
@@ -21,11 +21,13 @@ const userSchema = mongoose.Schema({
   },
 });
 
-userSchema.methods.generateHash =
-  password => bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+userSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(passsword, bcrypt.genSaltSync(8), null);
+};
 
-userSchema.methods.validPassword =
-  password => bcrypt.compareSync(password, this.local.password);
+userSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.local.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
 
