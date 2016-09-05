a53e83808fa8e7cb396592921e041a3c54b9ba0d
Sean Spradlin <seanspradlin@gmail.com>
2016-09-04 19:55:08 -0500

    tack on new dependencies

--- server/index.js
+++ server/index.js
@@ -1,7 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const passport = require('./lib/passport');
+const config = require('./config');
+const routes = require('./api');
 const app = express();
-app.use(express.static(`${__dirname}/../public`));
-app.listen(8080, () => {
-  console.log(`Server running at 8080`);
+
+mongoose.connect(config.db.url);
+
+app.use(helmet());
+app.use(express.static('../public'));
+app.use(cookieParser());
+app.use(bodyParser());
+app.use(session(config.session));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/api', routes);
+
+app.listen(config.port, () => {
+  console.log(`Server running on ${config.port}`);
 });
 
