e5bebaf6e57b8fe56bbe93c8bb348d014221a60c
Sean Spradlin <seanspradlin@gmail.com>
2016-09-04 22:13:15 -0500

    error docs

---
+++ server/api/_errors.js
@@ -0,0 +1,15 @@
+/**
+ * @apiDefine NotFoundError
+ * @apiError NotFound The resource could not be found.
+ */
+
+/**
+ * @apiDefine UnauthorizedError
+ * @apiError Unauthorized Insufficient credentials to access resource.
+ */
+
+/**
+ * @apiDefine UnprocessableEntityError
+ * @apiError UnprocessableEntity Invalid parameters were presented.
+ */
+
