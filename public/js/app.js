e34e0f8df7d5e38801beaca87a74a4d7a1d75809
Sean Spradlin <seanspradlin@gmail.com>
2016-09-05 22:40:46 -0500

    front-end test points

--- public/js/app.js
+++ public/js/app.js
@@ -10,3 +10,17 @@ new Vue({
   el: '#app',
 });
 
+$(document).ready(function() {
+  $.ajaxSetup({ cache: true });
+  $.getScript('//connect.facebook.net/en_US/sdk.js', function() {
+    FB.init({
+      appId: '157732058002990',
+      version: 'v2.7',
+    });
+    FB.getLoginStatus(function(response) {
+      $.post('api/auth/facebook', response.authResponse, 'json')
+        .then(console.log).fail(console.error);
+    });
+  });
+});
+
