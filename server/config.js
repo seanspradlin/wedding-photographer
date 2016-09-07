7e30844c351e9cd51a72212f71e51275859a1975
Sean Spradlin <seanspradlin@gmail.com>
2016-09-06 22:13:09 -0500

    google configs

--- server/config.js
+++ server/config.js
@@ -7,12 +7,16 @@ module.exports = {
     appId: process.env.FACEBOOK_CLIENT_ID || '',
     secret: process.env.FACEBOOK_CLIENT_SECRET || '',
   },
+  google: {
+    appId: process.env.GOOGLE_CLIENT_ID || '',
+    secret: process.env.GOOGLE_CLIENT_SECRET || '',
+  },
   logLevel: process.env.LOG_LEVEL || 'debug',
+  port: process.env.PORT || '8080',
   session: {
     secret: process.env.SESSION_SECRET || 'hunter2',
     resave: false,
     saveUninitialized: true,
   },
-  port: process.env.PORT || '8080',
 };
 
