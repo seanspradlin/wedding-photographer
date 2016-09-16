9486a461c56c3f830cdbe9d97579d37876c358ed
Sean Spradlin <seanspradlin@gmail.com>
2016-09-15 22:06:08 -0500

    bootstrapping

---
+++ client/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'wedding-photographer',
+    styleUrls: ['app/app.component.css'],
+    templateUrl: 'app/app.component.html'
+})
+export class AppComponent { }
+
